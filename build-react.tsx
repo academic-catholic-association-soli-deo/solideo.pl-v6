ff3022109917b266714c1eb366ad4a77b5bd73dd
Patryk Kuropatwiński <patryk.kuropatwinski@gmail.com>
2022-06-13 20:29:11 +0200

    added frontmatter parsing

--- build-react.tsx
+++ build-react.tsx
@@ -1,11 +1,13 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 import React, { ReactNode } from 'react'
 import ReactMarkdown from 'react-markdown'
-import children = ReactMarkdown.propTypes.children
+import yaml from 'yaml';
+import { string } from 'yaml/dist/schema/common/string'
 
-function MarkdownPage({ markdownContent }: {markdownContent: string}) {
+function MarkdownPage({ markdownContent, title }: {markdownContent: string, title: string}) {
     return (
         <Layout>
+          <h1>{title}</h1>
           <ReactMarkdown>
             {markdownContent}
           </ReactMarkdown>
@@ -25,8 +27,14 @@ function Layout({children}: {children: ReactNode}) {
 }
 
 export function markdownToHtml(markdownContent: string) {
-  return wrapHtml(renderToStaticMarkup(<MarkdownPage markdownContent={markdownContent}/>));
-}
+  const matchedGroups = /^---(?<frontmatter>[\s\S]*)---(?<markdown>[\s\S]*)$/gmy.exec(markdownContent);
+  const frontmatter = yaml.parse(matchedGroups?.groups?.frontmatter || '');
+  const markdown = matchedGroups?.groups?.markdown as string;
+  return wrapHtml({
+    html: renderToStaticMarkup(<MarkdownPage markdownContent={markdown} title={frontmatter.title}/>),
+    title: frontmatter.title,
+  })
+};
 
 function HomePage() {
   return (
@@ -36,12 +44,12 @@ function HomePage() {
   )
 }
 
-function wrapHtml(html: string): string {
+function wrapHtml({ html, title }: { html: string; title: string }): string {
   return (`<!DOCTYPE html>
-    <html lang="en">
+    <html lang="pl">
     <head>
         <meta charset="utf-8">
-        <title>Solideo</title>
+        <title>${title} - ASK Soli Deo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/style.css">
     </head>
@@ -53,5 +61,5 @@ function wrapHtml(html: string): string {
 }
 
 export function renderHomePage() {
-  return wrapHtml(renderToStaticMarkup(<HomePage/>));
+  return wrapHtml(  { html: renderToStaticMarkup(<HomePage/>), title: 'Strona główna'});
 }
